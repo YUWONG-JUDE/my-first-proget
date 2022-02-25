9459e4033c9755d03e8e73d930a343d82836f8fa
yuwong jude <jude.yuwong@fomicgroup.cm>
2022-02-25 13:44:15 +0100

    yuwong

---
+++ index.js
@@ -0,0 +1,17 @@
+ 
+   let js = 'amazing';
+   console.log(40+8+23 - 10); 
+
+    console. log("jude");
+    console. log("23");
+
+    let firstname = "jude";
+    console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+let person = "jones";
+let pi = 3.144;
+
+let myfirstjon ="programmer";
+let myscdon ="student";
